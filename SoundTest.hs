820ddc7aff288909f277e5422b4eac9d5e97ddea
Arnaud Bailly <Arnaud.Bailly@polyspot.com>
2012-01-31 19:15:14 +0100

    group tests in a test list

--- SoundTest.hs
+++ SoundTest.hs
@@ -2,7 +2,7 @@ import Test.HUnit
 import Sound
 
 -- first test on sound 
-convertAFrequencyToAWave = 
+convert_a_frequency_to_a_wave = 
   take 3 (wave frequency samplingRate) ~?= [0.0,6.283185307179587e-2,0.12566370614359174]
   where
     frequency    = 440
@@ -14,3 +14,8 @@ slice_a_wave_for_a_given_number_of_seconds =
     seconds = 2 
     samplingRate = 44000
     aWave   = wave 440 samplingRate
+
+tests = [ convert_a_frequency_to_a_wave, 
+          slice_a_wave_for_a_given_number_of_seconds]
+        
+runAllTests = runTestTT $ TestList tests
