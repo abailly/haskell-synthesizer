580ae8ca959bfd5cc03892758c232e23ce8ed433
Arnaud Bailly <Arnaud.Bailly@polyspot.com>
2012-01-31 18:54:42 +0100

    compute wave from a freq and sampling, first test pass

--- SoundTest.hs
+++ SoundTest.hs
@@ -1,5 +1,9 @@
 import Test.HUnit
+import Sound
 
 -- first test on sound 
 convertAFrequencyToAWave = 
-  take 3 (wave frequency samplingRate) ~?= [0.0, 0.1, 0.2]
+  take 3 (wave frequency samplingRate) ~?= [0.0,6.283185307179587e-2,0.12566370614359174]
+  where
+    frequency    = 440
+    samplingRate = 44000
