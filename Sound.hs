edeeca27de9cde9d5f791a9c6da806dda8317e33
Arnaud Bailly <Arnaud.Bailly@polyspot.com>
2012-01-31 19:01:00 +0100

    added type signatures and needed conversions
    * compare with inferred types in the REPL
    * Introduce numeric types in Haskell

--- Sound.hs
+++ Sound.hs
@@ -1,5 +1,6 @@
 module Sound where
 
+wave :: Integer -> Integer -> [Double]
 wave frequency samplingRate = 
-  let n = samplingRate / frequency
-  in map (* (2 * pi)) [ i / n | i <- [0 .. n]]
+  let n = samplingRate `div` frequency
+  in map (* (2 * pi)) [ fromIntegral i / fromIntegral n | i <- [0 .. n]]
