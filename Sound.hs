c7eb992ee009a04f505bbc0809906b63b5cb483d
Arnaud Bailly <Arnaud.Bailly@polyspot.com>
2012-01-31 19:21:51 +0100

    unit test for converting double values to single bytes
    * extract frequency rate to constant

--- Sound.hs
+++ Sound.hs
@@ -1,5 +1,8 @@
 module Sound where
 
+-- this a CAF: Constant Applicative Form
+samplingRate = 44000
+
 wave frequency samplingRate = 
   let n = samplingRate `div` frequency
   in map (sin . (* (2 * pi))) [ fromIntegral i / fromIntegral n | i <- [0 .. n]]
--- SoundTest.hs
+++ SoundTest.hs
@@ -6,14 +6,17 @@ convert_a_frequency_to_a_wave =
   take 3 (wave frequency samplingRate) ~?= [0.0,6.279051952931337e-2,0.12533323356430426]
   where
     frequency    = 440
-    samplingRate = 44000
 
 slice_a_wave_for_a_given_number_of_seconds = 
   length (slice seconds samplingRate aWave) ~?=  88000
   where
     seconds = 2 
-    samplingRate = 44000
     aWave   = wave 440 samplingRate
+
+scale_wave_to_a_single_byte_value =
+  take 3 (scale (0,255) wave) ~?= [0,1,2]
+  where
+    aWave = wave 440 samplingRate
 
 tests = [ convert_a_frequency_to_a_wave, 
           slice_a_wave_for_a_given_number_of_seconds]
