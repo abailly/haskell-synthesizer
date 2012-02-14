ef31f18085cd4fd8148b9a336240793ee23363a9
Arnaud Bailly <Arnaud.Bailly@polyspot.com>
2012-02-14 07:44:59 +0100

    added form handling, use package import to resolve ambiguity with monads transformers

--- websynth.hs
+++ websynth.hs
@@ -1,7 +1,9 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, PackageImports #-}
     
 import Network.Miku
 import Hack2.Handler.SnapServer
+import Hack2.Contrib.Request(params)
+import "monads-tf" Control.Monad.Reader
 import Text.Html   
 import qualified Data.ByteString.Char8 as B
 
@@ -19,3 +21,12 @@ main = run . miku $ do
                      ) ! [strAttr "action" "/synthesize",
                           strAttr "method" "GET"]
                     ])))
+  get "/synthesize" $ do 
+    env <- ask 
+    let (Just pars) = lookup "submit" $ params env
+    (html $
+     B.pack (prettyHtml $                   
+             thehtml 
+             (body $ concatHtml 
+              [h2 $ stringToHtml "Congratulations !",
+               stringToHtml $ B.unpack pars])))
