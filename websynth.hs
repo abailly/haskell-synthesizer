8d2247c3ae21d50cefb81968bfa99cc342503f68
Arnaud Bailly <Arnaud.Bailly@polyspot.com>
2012-02-14 07:34:08 +0100

    added basic form to submit scores

--- websynth.hs
+++ websynth.hs
@@ -2,5 +2,20 @@
     
 import Network.Miku
 import Hack2.Handler.SnapServer
-    
-main = run . miku $ get "/" (text "miku power")
+import Text.Html   
+import qualified Data.ByteString.Char8 as B
+
+main = run . miku $ do
+  get "/" (html $
+           B.pack (prettyHtml $
+                   thehtml 
+                   (body $ concatHtml 
+                    [h2 $ stringToHtml "Enter score",
+                     (form $ concatHtml [
+                         textarea noHtml,
+                         br,
+                         reset "cancel" "Cancel",
+                         submit "submit" "Submit"]
+                     ) ! [strAttr "action" "/synthesize",
+                          strAttr "method" "GET"]
+                    ])))
