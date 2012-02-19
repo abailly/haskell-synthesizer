d0c08e1fd888a15b32e6221a47c3124c44f488d9
Arnaud Bailly <Arnaud.Bailly@polyspot.com>
2012-02-19 21:15:19 +0100

    initial implementation of a simple function : converting a frequency to a sinusoidal sound wave

--- Sound.hs
+++ Sound.hs
@@ -3,19 +3,3 @@ module Sound where
 wave frequency samplingRate = 
   let n = samplingRate / frequency
   in map (* (2 * pi)) [ i / n | i <- [0 .. n]]
-
--- -- | Normalizes an interval [0,n] to [0,1]
--- normalizeDom :: Int -> [Float]
--- normalizeDom n = [(fromIntegral i) / n_ | i <- [0..n]]
---     where n_ = fromIntegral n
-
-
--- -- | Transforms [0,n] into [0,2*pi]
--- zeroTo2Pi :: Int -> [Float]
--- zeroTo2Pi n = map (* (2*pi)) (normalizeDom n)
-
-
--- -- | Synthesizes one period
--- period :: Int -> Waveform -> [Int]
--- period n Square = scale codomain $ map square (normalizeDom n)
--- period n Sine = scale codomain $ map sin (zeroTo2Pi n)
